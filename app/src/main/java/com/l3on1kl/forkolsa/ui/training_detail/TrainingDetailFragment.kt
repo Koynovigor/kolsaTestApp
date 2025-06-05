02f3d80880b1e5637f2771d9804bddcd9d2bb142
L3on1kL <igorkoynov_2001@gmail.com>
2025-06-05 17:15:27 +0700

    added full screen

--- app/src/main/java/com/l3on1kl/forkolsa/ui/training_detail/TrainingDetailFragment.kt
+++ app/src/main/java/com/l3on1kl/forkolsa/ui/training_detail/TrainingDetailFragment.kt
@@ -1,15 +1,12 @@
 package com.l3on1kl.forkolsa.ui.training_detail
 
-import android.os.Build
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.view.WindowInsets
-import android.view.WindowInsetsController
 import androidx.annotation.OptIn
-import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.activityViewModels
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.lifecycleScope
 import androidx.media3.common.MediaItem
@@ -18,10 +15,12 @@ import androidx.media3.common.Player
 import androidx.media3.common.VideoSize
 import androidx.media3.common.util.UnstableApi
 import androidx.media3.exoplayer.ExoPlayer
+import androidx.navigation.fragment.findNavController
 import androidx.navigation.fragment.navArgs
 import com.google.android.material.snackbar.Snackbar
 import com.l3on1kl.forkolsa.R
 import com.l3on1kl.forkolsa.databinding.FragmentTrainingDetailBinding
+import com.l3on1kl.forkolsa.ui.video.SharedPlayerViewModel
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.flow.collectLatest
 import kotlinx.coroutines.launch
@@ -33,9 +32,36 @@ class TrainingDetailFragment : Fragment() {
     private val binding get() = _binding!!
 
     private val viewModel: TrainingDetailViewModel by viewModels()
+    private val sharedPlayerViewModel: SharedPlayerViewModel by activityViewModels()
     private var player: ExoPlayer? = null
 
     private val args: TrainingDetailFragmentArgs by navArgs()
+
+    private val playerListener = object : Player.Listener {
+        override fun onPlaybackStateChanged(state: Int) {
+
+        }
+
+        override fun onPlayerError(error: PlaybackException) {
+            _binding?.root?.let {
+                Snackbar.make(it, "${error.message}", Snackbar.LENGTH_LONG).show()
+            }
+        }
+
+        override fun onVideoSizeChanged(videoSize: VideoSize) {
+            val width = videoSize.width
+            val height = videoSize.height
+            if (width > 0 && height > 0) {
+                val aspectRatio = height.toFloat() / width
+                _binding?.playerView?.post {
+                    val viewWidth = _binding?.playerView?.width ?: return@post
+                    val layoutParams = _binding?.playerView?.layoutParams ?: return@post
+                    layoutParams.height = (viewWidth * aspectRatio).toInt()
+                    _binding?.playerView?.layoutParams = layoutParams
+                }
+            }
+        }
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
@@ -72,108 +98,48 @@ class TrainingDetailFragment : Fragment() {
                 }
             }
         }
-
-
     }
 
     @OptIn(UnstableApi::class)
     private fun initPlayer(url: String) {
+        val localBinding = _binding ?: return
         val videoUrl = if (url.startsWith("http", ignoreCase = true)) {
             url
         } else {
             "https://ref.test.kolsa.ru$url"
         }
 
-        player?.release()
-        player = ExoPlayer.Builder(requireContext()).build().also { exo ->
-            binding.playerView.player = exo
+        val exo = sharedPlayerViewModel.player
+        player = exo
+        localBinding.playerView.player = exo
+
+        val currentMediaId = exo.currentMediaItem?.mediaId
+        if (currentMediaId != videoUrl) {
             val mediaItem = MediaItem.Builder()
                 .setUri(videoUrl)
+                .setMediaId(videoUrl)
                 .build()
+
             exo.setMediaItem(mediaItem)
             exo.prepare()
-            exo.addListener(object : Player.Listener {
-                override fun onPlaybackStateChanged(state: Int) {
+            exo.playWhenReady = true
 
-                }
-
-                override fun onPlayerError(error: PlaybackException) {
-
-                    Snackbar.make(
-                        binding.root,
-                        "${error.message}",
-                        Snackbar.LENGTH_LONG
-                    ).show()
-                }
-
-                override fun onVideoSizeChanged(videoSize: VideoSize) {
-                    val width = videoSize.width
-                    val height = videoSize.height
-                    if (width > 0 && height > 0) {
-                        val aspectRatio = height.toFloat() / width
-                        binding.playerView.post {
-                            val viewWidth = binding.playerView.width
-                            val layoutParams = binding.playerView.layoutParams
-                            layoutParams.height = (viewWidth * aspectRatio).toInt()
-                            binding.playerView.layoutParams = layoutParams
-                        }
-                    }
-                }
-            })
+            exo.removeListener(playerListener)
+            exo.addListener(playerListener)
         }
 
-        binding.playerView.setFullscreenButtonClickListener {
-            toggleFullscreen()
+        localBinding.playerView.setFullscreenButtonClickListener {
+            findNavController().navigate(
+                TrainingDetailFragmentDirections
+                    .actionTrainingDetailFragmentToFullscreenPlayerFragment(videoUrl)
+            )
         }
 
-        with(binding) {
-            playerView.controllerShowTimeoutMs = 3000
-            playerView.controllerAutoShow = true
+        with(localBinding.playerView) {
+            controllerShowTimeoutMs = 3000
+            controllerAutoShow = true
         }
-
     }
-
-    private var isFullscreen = false
-
-    private fun toggleFullscreen() {
-        val window = requireActivity().window
-        val decorView = window.decorView
-
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
-            // Android 11+ (API 30+)
-            val controller = window.insetsController ?: return
-            if (!isFullscreen) {
-                controller.hide(WindowInsets.Type.systemBars())
-                controller.systemBarsBehavior =
-                    WindowInsetsController.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE
-            } else {
-                controller.show(WindowInsets.Type.systemBars())
-            }
-        } else {
-            // Android 9–10 (API 28–29)
-            if (!isFullscreen) {
-                @Suppress("DEPRECATION")
-                decorView.systemUiVisibility = (
-                        View.SYSTEM_UI_FLAG_FULLSCREEN
-                                or View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
-                                or View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
-                        )
-            } else {
-                @Suppress("DEPRECATION")
-                decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_VISIBLE
-            }
-        }
-
-        val activity = requireActivity() as AppCompatActivity
-        if (!isFullscreen) {
-            activity.supportActionBar?.hide()
-        } else {
-            activity.supportActionBar?.show()
-        }
-
-        isFullscreen = !isFullscreen
-    }
-
 
     override fun onStop() {
         super.onStop()
@@ -181,7 +147,6 @@ class TrainingDetailFragment : Fragment() {
     }
 
     override fun onDestroyView() {
-        player?.release()
         player = null
         _binding = null
         super.onDestroyView()
